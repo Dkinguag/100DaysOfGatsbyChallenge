33071679a933bdecdacd92860661e3351a283606
kingd <dkinguag@gmail.com>
2020-01-29 00:21:55 -0600

    CreatedAboutPageUsingJSX

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,26 @@
 import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-
-export default ({ data }) => (
-  <Layout>
-    <h1>About {data.site.siteMetadata.title}</h1>
-    <p>
-      We're the only site running on your computer dedicated to showing the best
-      photos and videos of pandas eating lots of food.
-    </p>
-  </Layout>
-)
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "images/default.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
       }
     }
-  }
-`
+  `)
+  return (
+    <div>
+      <h1>Hello gatsby-image</h1>
+      <Img
+        fluid={data.file.childImageSharp.fluid}
+        alt="Gatsby Docs are awesome"
+      />
+    </div>
+  )
+}
